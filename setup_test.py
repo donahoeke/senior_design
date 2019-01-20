efcddc1332fdaef22ed8967c26fff3cf916526b0
amber-hack <43012968+amber-hack@users.noreply.github.com>
2019-01-20 11:04:25 -0600

    Create setup_test.py

---
+++ setup_test.py
@@ -0,0 +1,12 @@
+#goes to Desktop directory
+pi@raspberrypi $ cd Desktop/
+#creates a new directory called CameraPictures
+pi@raspberrypi ~/Desktop $ mkdir CameraPictures 
+#goes to CameraPictures directory
+pi@raspberrypi ~/Desktop $ cd CameraPictures
+#open a program to type in within the CameraPictures directory
+pi@raspberrypi ~/Desktop/CameraPictures $nano camexp.py
+#type program, control X to exit then hit Y and enter
+
+#execute program
+pi@raspberrypi ~/Desktop/CameraPictures $ python camexp.py
