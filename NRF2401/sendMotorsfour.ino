32b867df7c025b9638f82a798a6bf30e54fd8248
donahoeke <donahoeke@msoe.edu>
2019-03-11 11:13:09 -0500

    Update sendMotorsfour.ino

--- NRF2401/sendMotorsfour.ino
+++ NRF2401/sendMotorsfour.ino
@@ -4,37 +4,42 @@
 
 //Setting up the CE and CSN pins
 RF24 radio(9,10);
+
+//keeps track of last potentiomer value for comparison with new potentiometer values
 int old_potValue0 = 0;
 int old_potValue1 = 0;
 
-
 void setup(void){
-  Serial.begin(9600);
+  Serial.begin(9600); // begin the serial monitor for debugging purposes
   radio.begin();//Start the radio
   radio.setPALevel(RF24_PA_MAX);//Setting the Power Amplification Level
   radio.setChannel(0x76);//Setting channel
   radio.openWritingPipe(0xF0F0F0F0E1LL);//Open the sending address
   radio.enableDynamicPayloads();//Enable dynamic payloads
   radio.powerUp();//Power up the radio
-
-  Serial.println("You are Here!");
 }
 void loop(void) {
+  //read the potentiometer values
   int potValue0 = analogRead(A0);
   int potValue1 = analogRead(A1);
-  Serial.println("pv1 " + (String)potValue1);
+  
+  //converts the values to range from 3-12 (which is representative of duty cycle) instead of 0-1023, and appends a 1 or 2 
+  //to the front, based on whether it is potentiometer 1 or 2
   int angleValue0 = 100 + map(potValue0, 0, 1023, 3, 12);
   int angleValue1 = 200 + map(potValue1, 0, 1020, 3, 12);
-
-  Serial.println("av0 " + (String)potValue0 + "Old " + (String)old_potValue0);
-  Serial.println("av1 " + (String)potValue1 + "Old " + (String)old_potValue1);
   
+  //prints for debugging purposes
+  Serial.println("New av0 value: " + (String)potValue0 + "Old av0 value: " + (String)old_potValue0);
+  Serial.println("New av1 value: " + (String)potValue1 + "Old av1 value: " + (String)old_potValue1);
+  
+  //Sends a signal over radio if the new potentiometer 0 value is different than the old one (with a small range built in)
   if(potValue0 > old_potValue0 + 5 || potValue0 < old_potValue0-5)
   {
       radio.write(&angleValue0, sizeof(angleValue0));//Send the message
       delay(1200);
   }
-
+  
+  //Sends a signal over radio if the new potentiometer 1 value is different than the old one (with a small range built in)
   if(potValue1 > old_potValue1 + 5 || potValue1 < old_potValue1-5)
   {
       radio.write(&angleValue1, sizeof(angleValue1));//Send the message
@@ -44,5 +49,4 @@ void loop(void) {
 
   old_potValue0 = potValue0;
   old_potValue1 = potValue1;
-
 }
